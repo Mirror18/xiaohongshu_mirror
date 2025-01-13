d86a7824bc1946c2d0eac3209db9ff0c3582f219
mirror <mirrorcloud3@gmail.com>
2025-01-14 01:59:00 +0800

    自定义jackson配置，这里做了spring star 启动器，所以比较复杂一点

--- xiaohong-framework/xiaohong-common/src/main/java/com/mirror/framework/common/util/JsonUtils.java
+++ xiaohong-framework/xiaohong-common/src/main/java/com/mirror/framework/common/util/JsonUtils.java
@@ -18,20 +18,29 @@ import java.time.format.DateTimeFormatter;
  * @Description:
  */
 public class JsonUtils {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static  ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
         OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        OBJECT_MAPPER.registerModules(new JavaTimeModule()); // 解决 LocalDateTime 的序列化问题
+//        // JavaTimeModule 用于指定序列化和反序列化规则
+//        JavaTimeModule javaTimeModule = new JavaTimeModule();
+//
+//        // 支持 LocalDateTime
+//        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
+//        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
+//        // 解决 LocalDateTime 的序列化问题
+//        OBJECT_MAPPER.registerModules(javaTimeModule);
+    }
 
-        // JavaTimeModule 用于指定序列化和反序列化规则
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-
-        // 支持 LocalDateTime
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
-        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
-        // 解决 LocalDateTime 的序列化问题
-        OBJECT_MAPPER.registerModules(javaTimeModule);
+    /**
+     * 初始化：统一使用 Spring Boot 个性化配置的 ObjectMapper
+     *
+     * @param objectMapper
+     */
+    public static void init(ObjectMapper objectMapper) {
+        OBJECT_MAPPER = objectMapper;
     }
 
     /**
@@ -44,4 +53,6 @@ public class JsonUtils {
     public static String toJsonString(Object obj) {
         return OBJECT_MAPPER.writeValueAsString(obj);
     }
+
+
 }
--- xiaohongshu-auth/src/main/java/com/mirror/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/mirror/xiaohongshu/auth/controller/TestController.java
@@ -3,6 +3,8 @@ package com.mirror.xiaohongshu.auth.controller;
 import com.mirror.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.mirror.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -20,12 +22,9 @@ public class TestController {
         return Response.success("Hello, mirror");
     }
 
-    @GetMapping("/test2")
+    @PostMapping("/test2")
     @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2() {
-        return Response.success(User.builder()
-                .nickName("mirror")
-                .createTime(LocalDateTime.now())
-                .build());
+    public Response<User> test2(@RequestBody User user) {
+        return Response.success(user);
     }
 }
