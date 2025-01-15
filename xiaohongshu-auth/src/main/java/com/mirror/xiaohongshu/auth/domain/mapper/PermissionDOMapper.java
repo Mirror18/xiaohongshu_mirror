515456ed95441791fd33a56f9fe32f64c65d4ae1
mirror <mirrorcloud3@gmail.com>
2025-01-16 04:48:55 +0800

    同步角色权限集合数据到redis中，并且加redis锁，用于确保微服务只会执行一次

--- xiaohongshu-auth/src/main/java/com/mirror/xiaohongshu/auth/domain/mapper/PermissionDOMapper.java
+++ xiaohongshu-auth/src/main/java/com/mirror/xiaohongshu/auth/domain/mapper/PermissionDOMapper.java
@@ -2,6 +2,8 @@ package com.mirror.xiaohongshu.auth.domain.mapper;
 
 import com.mirror.xiaohongshu.auth.domain.dataobject.PermissionDO;
 
+import java.util.List;
+
 public interface PermissionDOMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +16,10 @@ public interface PermissionDOMapper {
     int updateByPrimaryKeySelective(PermissionDO record);
 
     int updateByPrimaryKey(PermissionDO record);
+    /**
+     * 查询 APP 端所有被启用的权限
+     *
+     * @return
+     */
+    List<PermissionDO> selectAppEnabledList();
 }
