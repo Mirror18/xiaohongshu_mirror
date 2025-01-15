f94912bfd4334e27f8a3c415ed119d3ec2ed1983
mirror <mirrorcloud3@gmail.com>
2025-01-15 10:08:48 +0800

    saToken整合Redis

--- xiaohongshu-auth/src/main/java/com/mirror/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/mirror/xiaohongshu/auth/controller/TestController.java
@@ -35,7 +35,7 @@ public class TestController {
         return Response.success(user);
     }
 
-    // 测试登录，浏览器访问： http://localhost:8080/user/doLogin?username=zhang&password=123456
+    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
     @RequestMapping("/user/doLogin")
     public String doLogin(String username, String password) {
         // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
@@ -46,7 +46,7 @@ public class TestController {
         return "登录失败";
     }
 
-    // 查询登录状态，浏览器访问： http://localhost:8080/user/isLogin
+    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
     @RequestMapping("/user/isLogin")
     public String isLogin() {
         return "当前会话是否登录：" + StpUtil.isLogin();
