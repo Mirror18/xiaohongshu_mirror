5d572e595c735c730a5ff2bdda8fc11a0701d9a5
mirror <mirrorcloud3@gmail.com>
2025-01-22 01:30:02 +0800

    gateway网关搭建，实现路由转发

---
+++ xiaohongshu-gateway/src/main/java/com/mirror/xiaohongshu/gateway/XiaohongshuGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.mirror.xiaohongshu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: mirror
+ * @Date: 2025/1/21 19:02
+ * @Description:
+ */
+@SpringBootApplication
+public class XiaohongshuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuGatewayApplication.class, args);
+    }
+
+}
