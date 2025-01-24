dba8f7c2c241ee0002d59e8d81b3dbe8684a8118
mirror <mirrorcloud3@gmail.com>
2025-01-24 08:48:26 +0800

    对象存储微服务搭建，主要是分为api和业务层，在后续更换业务的时候，不影响其他接口的更改

---
+++ xiaohongshu-oss/xiaohongshu-oss-biz/src/main/java/com/mirror/xiaohongshu/oss/biz/XiaohongshuOssBizAppilication.java
@@ -0,0 +1,16 @@
+package com.mirror.xiaohongshu.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: mirror
+ * @Date: 2025/1/24 08:35
+ * @Description:
+ */
+@SpringBootApplication
+public class XiaohongshuOssBizAppilication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuOssBizAppilication.class, args);
+    }
+}
