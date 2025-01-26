cb0832c32e1f32762ddc3802e38b4f37092d1c05
mirror <mirrorcloud3@gmail.com>
2025-01-27 06:48:16 +0800

    KV键值存储微服务搭建。另外本地需要安装好Cassandra

---
+++ xiaohongshu-kv/xiaohongshu-kv-biz/src/main/java/com/mirror/xiaohongshu/kv/biz/XiaohongshuKVBizApplication.java
@@ -0,0 +1,15 @@
+package com.mirror.xiaohongshu.kv.biz;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: mirror
+ * @Date: 2025/1/27 06:42
+ * @Description:
+ */
+@SpringBootApplication
+public class XiaohongshuKVBizApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(XiaohongshuKVBizApplication.class, args);
+    }
+}
