171024cd5bad8e5664660b85d089eac279c31e37
mirror <mirrorcloud3@gmail.com>
2025-01-28 04:27:24 +0800

    分布式ID生成服务整合leaf源码，顺便进行jmeter压力测试

---
+++ xiaohongshu-distributed-id-generator/xiaohongshu-distributed-id-generator-biz/src/main/java/com/mirror/xiaohongshu/distributed/id/generator/biz/XiaohongshuDistributedIdGeneratorBizApplication.java
@@ -0,0 +1,15 @@
+package com.mirror.xiaohongshu.distributed.id.generator.biz;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: mirror
+ * @Date: 2025/1/28 04:24
+ * @Description:
+ */
+@SpringBootApplication
+public class XiaohongshuDistributedIdGeneratorBizApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(XiaohongshuDistributedIdGeneratorBizApplication.class, args);
+    }
+}
