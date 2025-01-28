185b70db56eae26c79bb05e2b73759d0bdd23e89
mirror <mirrorcloud3@gmail.com>
2025-01-28 14:38:00 +0800

    笔记微服务搭建骨架

---
+++ xiaohongshu-note/xiaohongshu-note-biz/src/main/java/com/mirror/xiaohongshu/note/biz/XiaohongshuNoteBizApplication.java
@@ -0,0 +1,15 @@
+package com.mirror.xiaohongshu.note.biz;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: mirror
+ * @Date: 2025/1/28 14:29
+ * @Description:
+ */
+@SpringBootApplication
+public class XiaohongshuNoteBizApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(XiaohongshuNoteBizApplication.class, args);
+    }
+}
