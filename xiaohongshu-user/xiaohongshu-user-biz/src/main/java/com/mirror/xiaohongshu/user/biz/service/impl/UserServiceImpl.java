dcb2917231d05259d5fb951295ac397f2a3bd04a
mirror <mirrorcloud3@gmail.com>
2025-01-30 03:45:53 +0800

    通过引入redis缓存，用于提高用户信息访问速度增加，同时避免缓存穿透，redis加入了空数据

--- xiaohongshu-user/xiaohongshu-user-biz/src/main/java/com/mirror/xiaohongshu/user/biz/service/impl/UserServiceImpl.java
+++ xiaohongshu-user/xiaohongshu-user-biz/src/main/java/com/mirror/xiaohongshu/user/biz/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mirror.xiaohongshu.user.biz.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.google.common.base.Preconditions;
 import com.mirror.framework.biz.context.holder.LoginUserContextHolder;
 import com.mirror.framework.common.enums.DeletedEnum;
@@ -32,6 +33,7 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -41,6 +43,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Auther: mirror
@@ -278,6 +281,8 @@ public class UserServiceImpl implements UserService {
 
         return Response.success();
     }
+    @Resource(name = "taskExecutor")
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     /**
      * 根据用户 ID 查询用户信息
@@ -288,12 +293,29 @@ public class UserServiceImpl implements UserService {
     @Override
     public Response<FindUserByIdRspDTO> findById(FindUserByIdReqDTO findUserByIdReqDTO) {
         Long userId = findUserByIdReqDTO.getId();
+        // 用户缓存 Redis Key
+        String userInfoRedisKey = RedisKeyConstants.buildUserInfoKey(userId);
+        // 先从 Redis 缓存中查询
+        String userInfoRedisValue = (String) redisTemplate.opsForValue().get(userInfoRedisKey);
+        // 若 Redis 缓存中存在该用户信息
+        if (StringUtils.isNotBlank(userInfoRedisValue)) {
+            // 将存储的 Json 字符串转换成对象，并返回
+            FindUserByIdRspDTO findUserByIdRspDTO = JsonUtils.parseObject(userInfoRedisValue, FindUserByIdRspDTO.class);
+            return Response.success(findUserByIdRspDTO);
+        }
 
+        // 否则, 从数据库中查询
         // 根据用户 ID 查询用户信息
         UserDO userDO = userDOMapper.selectByPrimaryKey(userId);
 
         // 判空
         if (Objects.isNull(userDO)) {
+            threadPoolTaskExecutor.execute(() -> {
+                // 防止缓存穿透，将空数据存入 Redis 缓存 (过期时间不宜设置过长)
+                // 保底1分钟 + 随机秒数
+                long expireSeconds = 60 + RandomUtil.randomInt(60);
+                redisTemplate.opsForValue().set(userInfoRedisKey, "null", expireSeconds, TimeUnit.SECONDS);
+            });
             throw new BizException(ResponseCodeEnum.USER_NOT_FOUND);
         }
 
@@ -303,6 +325,14 @@ public class UserServiceImpl implements UserService {
                 .nickName(userDO.getNickname())
                 .avatar(userDO.getAvatar())
                 .build();
+
+        // 异步将用户信息存入 Redis 缓存，提升响应速度
+        threadPoolTaskExecutor.submit(() -> {
+            // 过期时间（保底1天 + 随机秒数，将缓存过期时间打散，防止同一时间大量缓存失效，导致数据库压力太大）
+            long expireSeconds = 60*60*24 + RandomUtil.randomInt(60*60*24);
+            redisTemplate.opsForValue()
+                    .set(userInfoRedisKey, JsonUtils.toJsonString(findUserByIdRspDTO), expireSeconds, TimeUnit.SECONDS);
+        });
 
         return Response.success(findUserByIdRspDTO);
     }
