788ea8d187a64737a3bbb0d0d1f06afb34da8004
mirror <mirrorcloud3@gmail.com>
2025-02-07 21:52:18 +0800

    将所有的计数服务都进行对齐，所谓的对齐操作，就是将我们原本存在的喜欢表，收藏表等都对齐于note的总数据汇总之中，当进行对齐的时候，同步更新redis中的数据。虽然看着提交的很多，但大部分都是重复性提交。而对齐操作是记录了都那些部分进行了操作，在夜间服务器不繁忙的时候，进行数据更新。一般来说是不会出差错的，但因为是MQ消费，所以会有队列等，计数服务没有进行幂等性操作，所以他们那只能保证有一个数据是对的，即插入的第一个表。

--- xiaohongshu-data-align/src/main/java/com/mirror/xiaohongshu/data/align/constant/RedisKeyConstants.java
+++ xiaohongshu-data-align/src/main/java/com/mirror/xiaohongshu/data/align/constant/RedisKeyConstants.java
@@ -17,6 +17,83 @@ public class RedisKeyConstants {
      */
     public static final String BLOOM_TODAY_NOTE_LIKE_USER_ID_LIST_KEY = "bloom:dataAlign:note:like:userIds";
 
+    /**
+     * 布隆过滤器：日增量变更数据，用户笔记收藏，取消收藏（笔记ID） 前缀
+     */
+    public static final String BLOOM_TODAY_NOTE_COLLECT_NOTE_ID_LIST_KEY = "bloom:dataAlign:note:collect:noteIds";
+
+    /**
+     * 布隆过滤器：日增量变更数据，用户笔记收藏，取消收藏（笔记发布者ID） 前缀
+     */
+    public static final String BLOOM_TODAY_NOTE_COLLECT_USER_ID_LIST_KEY = "bloom:dataAlign:note:collect:userIds";
+
+    /**
+     * 布隆过滤器：日增量变更数据，用户笔记发布，删除 前缀
+     */
+    public static final String BLOOM_TODAY_USER_NOTE_OPERATOR_LIST_KEY = "bloom:dataAlign:user:note:operators:";
+
+    /**
+     * 布隆过滤器：日增量变更数据，用户关注数 前缀
+     */
+    public static final String BLOOM_TODAY_USER_FOLLOW_LIST_KEY = "bloom:dataAlign:user:follows:";
+
+    /**
+     * 布隆过滤器：日增量变更数据，用户粉丝数 前缀
+     */
+    public static final String BLOOM_TODAY_USER_FANS_LIST_KEY = "bloom:dataAlign:user:fans:";
+
+    /**
+     * 用户维度计数 Key 前缀
+     */
+    private static final String COUNT_USER_KEY_PREFIX = "count:user:";
+
+    /**
+     * Hash Field: 关注总数
+     */
+    public static final String FIELD_FOLLOWING_TOTAL = "followingTotal";
+
+    /**
+     * 笔记维度计数 Key 前缀
+     */
+    private static final String COUNT_NOTE_KEY_PREFIX = "count:note:";
+
+    /**
+     * Hash Field: 笔记点赞总数
+     */
+    public static final String FIELD_LIKE_TOTAL = "likeTotal";
+
+    /**
+     * Hash Field: 笔记收藏总数
+     */
+    public static final String FIELD_COLLECT_TOTAL = "collectTotal";
+
+    /**
+     * Hash Field: 粉丝总数
+     */
+    public static final String FIELD_FANS_TOTAL = "fansTotal";
+
+    /**
+     * Hash Field: 笔记发布总数
+     */
+    public static final String FIELD_NOTE_TOTAL = "noteTotal";
+
+    /**
+     * 构建笔记维度计数 Key
+     * @param noteId
+     * @return
+     */
+    public static String buildCountNoteKey(Long noteId) {
+        return COUNT_NOTE_KEY_PREFIX + noteId;
+    }
+
+    /**
+     * 构建用户维度计数 Key
+     * @param userId
+     * @return
+     */
+    public static String buildCountUserKey(Long userId) {
+        return COUNT_USER_KEY_PREFIX + userId;
+    }
 
     /**
      * 构建完整的布隆过滤器：日增量变更数据，用户笔记点赞，取消点赞(笔记ID) KEY
@@ -36,20 +113,8 @@ public class RedisKeyConstants {
         return BLOOM_TODAY_NOTE_LIKE_USER_ID_LIST_KEY + date;
     }
 
-
     /**
-     * 布隆过滤器：日增量变更数据，用户笔记点赞，取消点赞（笔记ID） 前缀
-     */
-    public static final String BLOOM_TODAY_NOTE_COLLECT_NOTE_ID_LIST_KEY = "bloom:dataAlign:note:collect:noteIds";
-
-    /**
-     * 布隆过滤器：日增量变更数据，用户笔记点赞，取消点赞（笔记发布者ID） 前缀
-     */
-    public static final String BLOOM_TODAY_NOTE_COLLECT_USER_ID_LIST_KEY = "bloom:dataAlign:note:collect:userIds";
-
-
-    /**
-     * 构建完整的布隆过滤器：日增量变更数据，用户笔记点赞，取消点赞(笔记ID) KEY
+     * 构建完整的布隆过滤器：日增量变更数据，用户笔记收藏，取消收藏(笔记ID) KEY
      * @param date
      * @return
      */
@@ -58,18 +123,13 @@ public class RedisKeyConstants {
     }
 
     /**
-     * 构建完整的布隆过滤器：日增量变更数据，用户笔记点赞，取消点赞(笔记发布者ID) KEY
+     * 构建完整的布隆过滤器：日增量变更数据，用户笔记收藏，取消收藏(笔记发布者ID) KEY
      * @param date
      * @return
      */
     public static String buildBloomUserNoteCollectUserIdListKey(String date) {
         return BLOOM_TODAY_NOTE_COLLECT_USER_ID_LIST_KEY + date;
     }
-
-    /**
-     * 布隆过滤器：日增量变更数据，用户笔记发布，删除 前缀
-     */
-    public static final String BLOOM_TODAY_USER_NOTE_OPERATOR_LIST_KEY = "bloom:dataAlign:user:note:operators:";
 
     /**
      * 构建完整的布隆过滤器：日增量变更数据，用户笔记发布，删除 KEY
@@ -79,16 +139,6 @@ public class RedisKeyConstants {
     public static String buildBloomUserNoteOperateListKey(String date) {
         return BLOOM_TODAY_USER_NOTE_OPERATOR_LIST_KEY + date;
     }
-
-    /**
-     * 布隆过滤器：日增量变更数据，用户关注数 前缀
-     */
-    public static final String BLOOM_TODAY_USER_FOLLOW_LIST_KEY = "bloom:dataAlign:user:follows:";
-
-    /**
-     * 布隆过滤器：日增量变更数据，用户粉丝数 前缀
-     */
-    public static final String BLOOM_TODAY_USER_FANS_LIST_KEY = "bloom:dataAlign:user:fans:";
 
     /**
      * 构建完整的布隆过滤器：日增量变更数据，用户关注数 KEY
@@ -108,25 +158,5 @@ public class RedisKeyConstants {
         return BLOOM_TODAY_USER_FANS_LIST_KEY + date;
     }
 
-
-
-    /**
-     * 用户维度计数 Key 前缀
-     */
-    private static final String COUNT_USER_KEY_PREFIX = "count:user:";
-
-    /**
-     * Hash Field: 关注总数
-     */
-    public static final String FIELD_FOLLOWING_TOTAL = "followingTotal";
-
-    /**
-     * 构建用户维度计数 Key
-     * @param userId
-     * @return
-     */
-    public static String buildCountUserKey(Long userId) {
-        return COUNT_USER_KEY_PREFIX + userId;
-    }
 
 }
