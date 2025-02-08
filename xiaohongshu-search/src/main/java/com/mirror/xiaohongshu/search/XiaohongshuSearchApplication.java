cdc4e473ee23d02eb3c85188433387e13bb26fe8
mirror <mirrorcloud3@gmail.com>
2025-02-08 11:15:15 +0800

    搜索服务搭建，这里比较慢的原因是我在代码外，安装elasticsearch服务。比较麻烦，而且还需要学习一些语法。

---
+++ xiaohongshu-search/src/main/java/com/mirror/xiaohongshu/search/XiaohongshuSearchApplication.java
@@ -0,0 +1,15 @@
+package com.mirror.xiaohongshu.search;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: mirror
+ * @Date: 2025/2/8 11:11
+ * @Description:
+ */
+@SpringBootApplication
+public class XiaohongshuSearchApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(XiaohongshuSearchApplication.class, args);
+    }
+}
