ae03121fb2b4929b8736ceb280eab1c5a13de204
mirror <mirrorcloud3@gmail.com>
2025-02-19 17:56:14 +0800

    MQ消息发送可靠性保证，整合spring retry 充实框架 + 补偿发送方案。原先是依靠mq自带的重试次数，但是如果重试之后依旧无结果呢，所以需要补偿发送，虽然没做。但是框架已经写出，只需要落库并扫描重发即可。主要优化重试方案，现在的重试有随机时间。针对所有的MQ消息都可以这样处理

--- xiaohongshu-comment/xiaohongshu-comment-biz/src/main/java/com/mirror/xiaohongshu/comment/biz/XiaohongshuCommentBizApplication.java
+++ xiaohongshu-comment/xiaohongshu-comment-biz/src/main/java/com/mirror/xiaohongshu/comment/biz/XiaohongshuCommentBizApplication.java
@@ -3,6 +3,7 @@ package com.mirror.xiaohongshu.comment.biz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 /**
  * @Auther: mirror
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.mirror.xiaohongshu.comment.biz.domain.mapper")
+@EnableRetry // 启用 Spring Retry
 public class XiaohongshuCommentBizApplication {
     public static void main(String[] args) {
         SpringApplication.run(XiaohongshuCommentBizApplication.class, args);
--- xiaohongshu-comment/xiaohongshu-comment-biz/src/main/java/com/mirror/xiaohongshu/comment/biz/server/impl/CommentServiceImpl.java
+++ xiaohongshu-comment/xiaohongshu-comment-biz/src/main/java/com/mirror/xiaohongshu/comment/biz/server/impl/CommentServiceImpl.java
@@ -7,15 +7,12 @@ import com.mirror.framework.common.util.JsonUtils;
 import com.mirror.xiaohongshu.comment.biz.constant.MQConstants;
 import com.mirror.xiaohongshu.comment.biz.model.dto.PublishCommentMqDTO;
 import com.mirror.xiaohongshu.comment.biz.model.vo.PublishCommentReqVO;
+import com.mirror.xiaohongshu.comment.biz.retry.SendMqRetryHelper;
 import com.mirror.xiaohongshu.comment.biz.server.CommentService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.rocketmq.client.producer.SendCallback;
-import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -32,6 +29,8 @@ public class CommentServiceImpl implements CommentService {
     @Resource
     private RocketMQTemplate rocketMQTemplate;
 
+    @Resource
+    private SendMqRetryHelper sendMqRetryHelper;
     /**
      * 发布评论
      *
@@ -63,22 +62,8 @@ public class CommentServiceImpl implements CommentService {
                 .creatorId(creatorId)
                 .build();
 
-        // 构建消息对象，并将 DTO 转成 Json 字符串设置到消息体中
-        Message<String> message = MessageBuilder.withPayload(JsonUtils.toJsonString(publishCommentMqDTO))
-                .build();
-
-        // 异步发送 MQ 消息，提升接口响应速度
-        rocketMQTemplate.asyncSend(MQConstants.TOPIC_PUBLISH_COMMENT, message, new SendCallback() {
-            @Override
-            public void onSuccess(SendResult sendResult) {
-                log.info("==> 【评论发布】MQ 发送成功，SendResult: {}", sendResult);
-            }
-
-            @Override
-            public void onException(Throwable throwable) {
-                log.error("==> 【评论发布】MQ 发送异常: ", throwable);
-            }
-        });
+        // 发送 MQ (包含重试机制)
+        sendMqRetryHelper.asyncSend(MQConstants.TOPIC_PUBLISH_COMMENT, JsonUtils.toJsonString(publishCommentMqDTO));
 
         return Response.success();
     }
